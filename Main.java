9d4514df60bc44949e16c1e5a5add35d7c119683
Sival Junior <jr_sival@hotmail.com>
2015-03-07 16:05:11 -0300

    Criação da Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main{
+
+  public static void main(String args[]){
+    Retangulo r1;
+    r1 = new Retangulo();
+    r1.x = 10;
+    r1.y = 12;
+    System.out.println("Area = " + r1.area());
+    System.out.println("Perimetro = " + r1.perimetro());
+  }
+
+}
